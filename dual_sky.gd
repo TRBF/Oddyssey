9f00b4d99704b727cacad01b6d53ca24b1b6348f
TRBF <trbf.contact@gmail.com>
2023-07-19 00:04:29 +0300

    Added text prompts

--- dual_sky.gd
+++ dual_sky.gd
@@ -3,7 +3,8 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$CanvasLayer/TextPrompts/WarnSeparation.hide()
+	$CanvasLayer/TextPrompts/NextLevelIndication.hide()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -14,3 +15,23 @@ func _process(delta):
 func _on_fall_prevention_body_entered(body):
 	$Player.position = Vector2(25, 850)
 #	$Player.velocity = Vector2(0, -150)
+
+
+func _on_warning_body_entered(body):
+	if(body.name == "Player"):
+		$CanvasLayer/TextPrompts/WarnSeparation.show()
+
+
+func _on_warning_body_exited(body):
+	if(body.name == "Player"):
+		$CanvasLayer/TextPrompts/WarnSeparation.hide()
+	
+
+func _on_next_level_instructions_body_entered(body):
+	if(body.name == "Player"):
+		$CanvasLayer/TextPrompts/NextLevelIndication.show()
+
+
+func _on_next_level_instructions_body_exited(body):
+	if(body.name == "Player"):
+		$CanvasLayer/TextPrompts/NextLevelIndication.hide()
